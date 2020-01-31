4831a5fde457daf42ff9db8c876f11a54cfeef26
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-31 13:49:44 -0500

    tests for auth router

--- auth/auth-router.test.js
+++ auth/auth-router.test.js
@@ -1,20 +1,36 @@
 const db = require('../database/dbConfig.js');
-const Users = require('./auth-router.js');
+const Users = require('./authModel.js');
 
 
 describe ('register and login', () => {
-    beforeEach(async () => {
-        await db('users').truncate;
-    });
+    
+    describe('test environment', function() {
+        it('should use the testing environment', function() {
+            expect(process.env.DB_ENV).toBe('testing');
+        })
+    })
 
-    describe('insert()', () => {
+    describe('insert() is working for registering', () => {
+
+        beforeEach(async () => {
+            await db('users').truncate();
+        })
+
         it('should insert a new user into the database', async () => {
-            await Users.insert({name: 'ariana'});
-            await Users.insert({name: 'alec'});
+            await Users.insert({username: 'ariana', password: 'devon'});
+            await Users.insert({username: 'alec', password: 'oren'});
 
             const users = await db('users');
 
             expect(users).toHaveLength(2);
         });
-    });
+    })
+    describe('findBy() is working for login', () => {
+        it('should find user from username', async () => {
+           const user = await Users.findBy({username: 'ariana'});
+
+           expect.arrayContaining(user);
+        })
+    })
+   
 })
