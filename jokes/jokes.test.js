0d1a9ac8ac08c3f32f392251733934bbaecadb88
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-31 13:50:44 -0500

    tests for jokes

--- jokes/jokes.test.js
+++ jokes/jokes.test.js
@@ -1,2 +1,19 @@
 const db = require('../database/dbConfig.js');
-const Jokes
+const jokes = require('./jokes-router.js');
+const request = require('supertest');
+
+
+describe('jokes', () => {
+    it('runs tests', () => {
+        expect(true).toBe(true);
+    })
+
+    describe('Get /jokes', () => {
+        it('return status 200', () => {
+            return request(jokes).get('/api/jokes')
+                .then(res => {
+                    expect(res.status).toBe(200)
+                })
+        })
+    } )
+})
