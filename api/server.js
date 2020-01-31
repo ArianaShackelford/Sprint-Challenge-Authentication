2a3662a785429391219bc1b439f9d42c91f6d13c
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-31 13:49:04 -0500

    sessions added

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
@@ -11,6 +12,19 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+
+
+server.use(session({
+    name: 'cessaion',
+    secret: 'Never go against a Sicilian when DEATH is on the line!',
+    cookie: {
+        maxAge:  90 *1000,
+        secure: false, // changed to true if in production
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false, //set to false, will later be dynamically changed. GDPR laws
+}));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
